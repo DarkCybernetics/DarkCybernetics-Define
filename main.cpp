a525f41e23b449e6603627401a312cce25c0b784
DarkCybernetics <Dion.jacksoncs@yahoo.com>
2019-11-29 12:19:09 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,22 @@
+// Define Macro
+// www.DarkCybernetics.com
+// All Rights Reserved 2019.
+
+#include <iostream>
+#define Number 9
+#define Product 8*7
+
+using namespace std;
+
+int main(){
+
+cout<<" Numbers "<<Number<<endl;
+cout<<" Product "<<Product<<endl;
+
+int car = 9;
+int car_price = Product * Number * car;
+
+cout<<car_price;
+
+ return 0;
+}
